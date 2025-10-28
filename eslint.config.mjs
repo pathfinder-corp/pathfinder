1353fce516d03c5f693b99da610c0b3415a8ba43
Lam Quoc Hung <68631787+hlam812003@users.noreply.github.com>
2025-10-28 11:23:21 +0700

    feat: update configuration and code prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config';
 import perfectionist from 'eslint-plugin-perfectionist'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -23,21 +24,22 @@ export default tseslint.config(
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
-    },
+    }
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-    },
+      '@typescript-eslint/comma-dangle': ['error', 'never']
+    }
   },
   {
     plugins: {
-      perfectionist,
+      perfectionist
     },
     rules: {
-      'perfectionist/sort-imports': 'error',
+      'perfectionist/sort-imports': 'error'
     },
   },
 )
---
+++ src/config/app.config.ts
@@ -0,0 +1,17 @@
+export default () => ({
+  port: parseInt(process.env.PORT ?? '8000', 10) || 8000,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10) || 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  }
+  // jwt: {
+  //   secret: process.env.JWT_SECRET,
+  //   expiresIn: process.env.JWT_EXPIRATION
+  // },
+  // ai: {
+  //   geminiKey: process.env.GEMINI_API_KEY
+  // }
+})
